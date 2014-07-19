811afd9bf2bb66d0372e30460c9939b14042443c
diek <diek@bellaliant.net>
2014-07-19 02:22:47 -0300

    1 mod, added file

---
+++ get_scores.py
@@ -0,0 +1,19 @@
+#Python 2.7
+''' Requests is already installed for Python 3 but not for python 2.7 you need
+to install requests; for linux or mac users, the easiest method is to install
+using python's package manager pip, so: $ sudo pip install requests
+for windows install see the accepted answer:
+http://stackoverflow.com/questions/21493784/python-importerror-no-module-named-requests
+'''
+import requests
+resp = requests.get('http://worldcup.sfg.io/matches')
+for jogo in resp.json():
+    if jogo['status'] == 'completed':
+        print jogo['home_team']['country'], jogo['home_team']['goals'], 'x', jogo['away_team']['country'], jogo['away_team']['goals']
+
+# Python 3
+# import urllib.request
+# import json
+# resp = urllib.request.urlopen('http://worldcup.sfg.io/matches').read()
+# for jogo in json.loads(resp.decode('utf-8')):
+#     if jogo['status'] == 'completed':
